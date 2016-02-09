63b92401f7a8e9ef2a96e0e1ef2e9ddc698e6f11
Alex <turnerawuindy@gmail.com>
2016-02-09 17:03:09 -0500

    first payroll update

--- payrole.cpp
+++ payrole.cpp
@@ -4,7 +4,7 @@ using namespace std;
  
 int main() { 
 //setting up variables
-double hours, rate, pay; 
+double hours, rate, pay, statetax, netpay, social, medicare; 
  
 // Get the number of hours worked. 
 cout << "How many hours did you work? "; 
@@ -17,7 +17,22 @@ cin >> rate;
 // Calculate the pay. 
 pay = hours * rate; 
  
-// Display the pay. 
-cout << "You have earned $" << pay << endl; 
+// Calculate State tax. 
+statetax = (pay * .034);
+
+// Calculate Social Security
+social = (pay * .062);
+  
+// Calculate Medicare
+medicare = (pay * .0145);
+  
+// Calculate net pay.
+netpay = pay - (statetax + social + medicare);
+  
+// Display gross pay
+cout << "Your gross pay is " << pay << endl;
+  
+// Display the netpay. 
+cout << "You're net pay is " << netpay << endl; 
 return 0; 
 }
